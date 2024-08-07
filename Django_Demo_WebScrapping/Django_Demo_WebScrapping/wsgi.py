d7a19b561d355cf4a6f9e65c6c949f555e4626df
Lrusso <leandro.russo288@gmail.com>
2024-08-07 20:10:21 -0300

    prueba de settings

--- Django_Demo_WebScrapping/Django_Demo_WebScrapping/wsgi.py
+++ Django_Demo_WebScrapping/Django_Demo_WebScrapping/wsgi.py
@@ -8,10 +8,9 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ["DJANGO_SETTINGS_MODULE"] = "Django_Demo_WebScrapping.settings"
+os.environ["DJANGO_SETTINGS_MODULE"] = "settings"
 # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_Demo_WebScrapping.settings')
 
 application = get_wsgi_application()
