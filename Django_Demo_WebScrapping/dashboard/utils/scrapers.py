77b4cc971dba521915a89158460daeb7126a7b73
Lrusso <leandro.russo288@gmail.com>
2024-08-21 19:45:55 -0300

    Se modulariza la función de run_scraper

---
+++ Django_Demo_WebScrapping/dashboard/utils/scrapers.py
@@ -0,0 +1,21 @@
+import logging
+from Demo_WebScrapping import main as scraper_main
+from django.http import HttpResponse
+from django.conf import settings
+
+logger = logging.getLogger('django')
+
+def run_scraper(request, scraper_name):
+    if not settings.IS_LOCAL:
+        return HttpResponse('This feature is available only for local executions or with a subscription.', status=403)
+
+    try:
+        # Llamada directa al main.py con el parámetro correspondiente
+        scraper_main.main(scraper_name)
+        
+        # Log de éxito
+        logger.info(f"{scraper_name} scraper executed successfully")
+        return HttpResponse(f"{scraper_name} scraper executed successfully")
+    except Exception as e:
+        logger.error(f"Error during execution: {str(e)}")
+        return HttpResponse(f"Error: {str(e)}", status=500)
--- Django_Demo_WebScrapping/dashboard/views.py
+++ Django_Demo_WebScrapping/dashboard/views.py
@@ -2,12 +2,8 @@ import subprocess
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.conf import settings
-import os
-import sys
-import platform
 import logging
-import datetime
-from Demo_WebScrapping import main as scraper_main
+from .utils.scrapers import run_scraper
 
 
 
@@ -17,31 +13,7 @@ def index(request):
     return render(request, 'index.html')
 
 def run_fill_form(request):
-    if not settings.IS_LOCAL:
-        return HttpResponse('This feature is available only for local executions or with a subscription.', status=403)
-
-    try:
-        # Llamada directa al main.py con el parámetro 'FillForm'
-        scraper_main.main('FillForm')
-        
-        # Log de éxito
-        logger.info("FillForm scraper executed successfully")
-        return HttpResponse("FillForm scraper executed successfully")
-    except Exception as e:
-        logger.error(f"Error during execution: {str(e)}")
-        return HttpResponse(f"Error: {str(e)}", status=500)
+    return run_scraper(request, 'FillForm')
 
 def run_table_scraper(request):
-    if not settings.IS_LOCAL:
-        return HttpResponse('This feature is available only for local executions or with a subscription.', status=403)
-
-    try:
-        # Llamada directa al main.py con el parámetro 'TableScrapper'
-        scraper_main.main('TableScrapper')
-        
-        # Log de éxito
-        logger.info("TableScrapper executed successfully")
-        return HttpResponse("TableScrapper executed successfully")
-    except Exception as e:
-        logger.error(f"Error during execution: {str(e)}")
-        return HttpResponse(f"Error: {str(e)}", status=500)
+    return run_scraper(request, 'TableScrapper')
